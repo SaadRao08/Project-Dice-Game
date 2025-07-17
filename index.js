0ebb5af9d1b4e018459d451b316ceaec0df1a22f
SaadRao08 <150579598+SaadRao08@users.noreply.github.com>
2025-07-17 10:39:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+var randomimage = "images/dice" + randomnumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomimage);
+
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+var randomimagesource = "images/dice" + randomnumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomimagesource);
+
+if(randomnumber1 > randomnumber2){
+    document.querySelector("h1").innerHTML=" Player 1 wins 🎲";
+}else if(randomnumber2 > randomnumber1){
+    document.querySelector("h1").innerHTML="Player 2 wins 🎲";
+}else{
+    document.querySelector("h1").innerHTML="Match Draw 🎌";
+}
+
